a8207d09c924e6369c4d681d6d02f971a61a5e1c
Louioskarmen <ayxc000@qq.com>
2025-05-07 14:19:48 +0800

    5.7.2

--- CSDN项目/csdn(前端）-vue3/src/main.ts
+++ CSDN项目/csdn(前端）-vue3/src/main.ts
@@ -1,14 +1,10 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
-import router from './router'
+import { createPinia } from 'pinia'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
+const pinia = createPinia()
 
 app.mount('#app')
+app.use(pinia)
