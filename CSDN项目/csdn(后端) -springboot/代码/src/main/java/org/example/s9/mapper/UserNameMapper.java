8d6060a74bcc5ecd76218c4612989f7fce3d03e7
Louioskarmen <ayxc000@qq.com>
2025-05-21 20:02:34 +0800

    5.21.1

--- CSDN项目/csdn(后端) -springboot/代码/src/main/java/org/example/s9/mapper/UserNameMapper.java
+++ CSDN项目/csdn(后端) -springboot/代码/src/main/java/org/example/s9/mapper/UserNameMapper.java
@@ -1,5 +1,6 @@
 package org.example.s9.mapper;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.example.s9.pojo.user;
@@ -9,4 +10,7 @@ import org.example.s9.pojo.user;
 public interface UserNameMapper {
     @Select("select * from userdoro where name=#{name}")
     user getUserByName(String name);
+    @Insert("insert into userdoro values(#{name},#{password},#{img},#{id}")
+    void  addUser(user user);
+
 }
