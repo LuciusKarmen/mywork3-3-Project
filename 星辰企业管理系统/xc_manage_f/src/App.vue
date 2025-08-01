bacbaca5ca9186e1bc2c708e269e279dd4e01450
karmen <166833697+LuciusKarmen@users.noreply.github.com>
2025-08-01 10:16:20 +0800

    8.1.1

--- 星辰企业管理系统/xc_manage_f/src/App.vue
+++ 星辰企业管理系统/xc_manage_f/src/App.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-import { XcButton } from 'xingchengnet-ui'
-</script>
-
+<script setup lang="ts"></script>
+import
 <template>
   <xc-button type="danger">发布成功！</xc-button>
+  <xc-input type="danger">1</xc-input>
 </template>
 
 <style scoped></style>
